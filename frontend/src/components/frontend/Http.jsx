c3da09efd9307c2ea137776d104f29507bf53c89
cseishrakul <ishrakul236@gmail.com>
2025-07-15 02:30:06 +0600

    Frontend trying to upload

--- frontend/src/components/frontend/Http.jsx
+++ frontend/src/components/frontend/Http.jsx
@@ -1,5 +1,5 @@
-export const apiurl = "http://localhost:8000/api/";
-export const fileurl = "http://localhost:8000/";
+export const apiurl = "https://construction-aqri.onrender.com/api/";
+export const fileurl = "https://construction-aqri.onrender.com/";
 export const token = () => {
   const userInfo = localStorage.getItem("userInfo");
   const data = JSON.parse(userInfo);
