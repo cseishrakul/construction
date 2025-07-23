69675892c30a9b6e7fc35bf6de55ccfc2010a043
cseishrakul <ishrakul236@gmail.com>
2025-07-23 16:43:07 +0600

    Finally done

--- frontend/src/pages/backend/Login.jsx
+++ frontend/src/pages/backend/Login.jsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { AuthContext } from "../../components/backend/context/Auth";
-import "./Login.css";
+import "./login.css";
 import { apiurl } from "../../components/frontend/Http";
 
 const Login = () => {
